e72d55595351f931d11b276d4cf7fd9f829946b0
TaimonFix <bratyshev2013@mail.ru>
2024-07-26 12:49:51 +0300

    Создание репозитория fileStorage

---
+++ src/main/java/ru/rosatom/edu/bratyshevTD/fileStorage/FileStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.rosatom.edu.bratyshevTD.fileStorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageApplication.class, args);
+	}
+
+}
